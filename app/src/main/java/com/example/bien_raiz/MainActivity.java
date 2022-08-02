498127aafbcb92e96c9c2bb9038dc086793e8bb6
marsanchezs <marsanchezs@gmail.com>
2022-08-02 19:01:16 -0400

    add branch name develop 4 times

--- app/src/main/java/com/example/bien_raiz/MainActivity.java
+++ app/src/main/java/com/example/bien_raiz/MainActivity.java
@@ -31,7 +31,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Log.d("Branch", "develop");
-
-
+        Log.d("Branch", "develop");
+        Log.d("Branch", "develop");
+        Log.d("Branch", "develop");
     }
 }
