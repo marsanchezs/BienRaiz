97a19f62f1f7cbc73d59aa5cc35f7b70ada6ae3c
marsanchezs <marsanchezs@gmail.com>
2022-08-02 18:55:39 -0400

    add branch name develop

--- app/src/main/java/com/example/bien_raiz/MainActivity.java
+++ app/src/main/java/com/example/bien_raiz/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.d("Branch", "rebase");
+        Log.d("Branch", "develop");
 
 
     }
